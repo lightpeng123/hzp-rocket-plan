6f14f12731910f5b9c15869b84c4b52519e7a69b
lightpeng123 <lightpeng123@163.com>
2019-05-28 09:59:48 +0800

    Add files via upload

---
+++ src/main/java/com/hzp/HzpRocketPlanApplication.java
@@ -0,0 +1,13 @@
+package com.hzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzpRocketPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzpRocketPlanApplication.class, args);
+    }
+
+}
